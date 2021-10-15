634bad4536ffa4169b9c3b61713099cfa155ab09
stefanerefrande <stefanerefrande@gmail.com>
2021-10-15 02:02:39 -0300

    #2 Creates models

--- CompanyManagement/CompanyApp/models.py
+++ CompanyManagement/CompanyApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Employees(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    employee_name = models.CharField(max_length=100)
+    employee_username = models.CharField(max_length=50)
+
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=300)
+    employee = models.ManyToManyField(Employees)
